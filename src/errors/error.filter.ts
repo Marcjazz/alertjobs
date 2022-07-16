d190df06f94fa198129cc4affdc8b9f4bbfe58c7
Marco Kuidja <kuidjamarco@gmail.com>
2022-07-16 11:44:35 +0100

    adds

--- src/errors/error.filter.ts
+++ src/errors/error.filter.ts
@@ -3,15 +3,16 @@ import {
   ArgumentsHost,
   ExceptionFilter,
   HttpException,
+  NotFoundException,
 } from '@nestjs/common';
 import { Response } from 'express';
 
-@Catch(HttpException)
+@Catch(HttpException, NotFoundException)
 export class ErrorFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    console.log(exception)
+    console.log(exception);
     response.render('errors/500', {
       message: exception.getResponse(),
     });
